524d8b1f029df375dcefcbe6d0594be2015cb9d3
valentin_noske <schaefer.noske@gmail.com>
2021-11-14 22:46:22 +0100

    run cpp geloescht

---
+++ include/networkit/algebraic/algorithms/SpGEMM.cpp
@@ -0,0 +1,5 @@
+//
+// Created by valentin on 14.11.21.
+//
+
+#include "SpGEMM.h"
---
+++ include/networkit/algebraic/algorithms/SpGEMM.h
@@ -0,0 +1,14 @@
+//
+// Created by valentin on 14.11.21.
+//
+
+#ifndef NETWORKIT_SPGEMM_H
+#define NETWORKIT_SPGEMM_H
+
+
+class SpGEMM {
+
+};
+
+
+#endif //NETWORKIT_SPGEMM_H
